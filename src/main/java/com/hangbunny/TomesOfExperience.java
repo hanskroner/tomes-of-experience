51f2119a912e98c7ccb7e370dd45bc0ef4bb96ac
Hans Kröner <hans.kroner@gmail.com>
2024-03-18 10:49:15 +0100

    Add base 'Tome of Experience'

--- src/main/java/com/hangbunny/TomesOfExperience.java
+++ src/main/java/com/hangbunny/TomesOfExperience.java
@@ -1,6 +1,12 @@
 package com.hangbunny;
 
+import com.hangbunny.item.TomeOfExperience;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,12 +17,15 @@ public class TomesOfExperience implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("tomes-of-experience");
 
+	// Base Tome of Experience Item
+	public static final Item TomeOfExperience = new TomeOfExperience(new FabricItemSettings());
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ITEM, new Identifier("tomes_of_experience", "tome_of_experience"), TomeOfExperience);
 	}
 }
---
+++ src/main/java/com/hangbunny/item/TomeOfExperience.java
@@ -0,0 +1,10 @@
+package com.hangbunny.item;
+
+import net.minecraft.item.Item;
+
+public class TomeOfExperience extends Item {
+ 
+    public TomeOfExperience(Settings settings) {
+        super(settings);
+    }
+}
