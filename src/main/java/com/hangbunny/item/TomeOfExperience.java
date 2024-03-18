0359acca2b4f53deff4865cd5f9599060c9fceaa
Hans Kröner <hans.kroner@gmail.com>
2024-03-18 10:59:41 +0100

    Add a localized tooltip for 'Tome of Experience'

--- src/main/java/com/hangbunny/item/TomeOfExperience.java
+++ src/main/java/com/hangbunny/item/TomeOfExperience.java
@@ -1,10 +1,20 @@
 package com.hangbunny.item;
 
+import java.util.List;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
 
 public class TomeOfExperience extends Item {
  
     public TomeOfExperience(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(Text.translatable("item.tomes_of_experience.tome_of_experience.tooltip.empty"));
     }
 }
