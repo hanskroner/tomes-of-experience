8938c7400faa01351c86b56c7757426b327c3af3
Hans Kröner <hans.kroner@gmail.com>
2024-03-19 18:09:24 +0100

    Add 'Tome of Experience' to the Creative inventory
    The tome will be listed under the 'Ingredients' tab.

--- src/main/java/com/hangbunny/TomesOfExperience.java
+++ src/main/java/com/hangbunny/TomesOfExperience.java
@@ -7,7 +7,9 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -38,5 +40,9 @@ public class TomesOfExperience implements ModInitializer {
 
 		// Items
 		Registry.register(Registries.ITEM, new Identifier("tomes_of_experience", "tome_of_experience"), TomeOfExperience);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+			content.add(TomeOfExperience);
+		});
 	}
 }
