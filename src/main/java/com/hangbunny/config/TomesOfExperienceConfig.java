e3c51b31bfd87f05e3fe290b99acc277102f93c8
Hans Kröner <hans.kroner@gmail.com>
2024-03-19 07:39:19 +0100

    Add basic mod configuration
    This also provides integration with Mod Menu, allowing the mod
    to be configured from within the Minecraft client.

---
+++ src/main/java/com/hangbunny/config/TomesOfExperienceConfig.java
@@ -0,0 +1,13 @@
+package com.hangbunny.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(name = "tomes_of_experience")
+@Config.Gui.Background("minecraft:textures/block/stone.png")
+public class TomesOfExperienceConfig implements ConfigData {
+
+    @Comment("The maximum amount of experience points a tome can hold")
+    public int experience_points_capacity = 1395;
+}
