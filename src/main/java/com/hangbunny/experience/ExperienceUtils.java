cf014d139bcf258029061cdf165678654bbbafad
Hans Kröner <hans.kroner@gmail.com>
2024-03-23 11:42:26 +0100

    Change rounding behavior and level points calculation
    These changes make it easier for tome subclasses to reason about
    experience point calculations when transferring experience points.

--- src/main/java/com/hangbunny/experience/ExperienceUtils.java
+++ src/main/java/com/hangbunny/experience/ExperienceUtils.java
@@ -8,13 +8,13 @@ public class ExperienceUtils {
 		int experienceCurrentLevel = ExperienceUtils.getExperienceForLevel(user.experienceLevel);
 		int experienceForNextLevel = ExperienceUtils.getExperienceForLevel(user.experienceLevel + 1) - experienceCurrentLevel;
 
-		return (int)(experienceCurrentLevel + (user.experienceProgress * experienceForNextLevel));
+		return (int) Math.round((experienceCurrentLevel + (user.experienceProgress * experienceForNextLevel)));
 	}
 
 	public static int getExperienceForLevel(int level) {
 		// https://minecraft.fandom.com/wiki/Experience#Leveling_up
 
-		if (level ==  0) { return 0; }
+		if (level <=  0) { return 0; }
 		if (level <= 16) { return (int) (Math.pow(level, 2) + 6 * level); }
 		if (level <= 31) { return (int) (2.5 * Math.pow(level, 2) - (40.5 * level) + 360); }
 		return (int) (4.5 * Math.pow(level, 2) - (162.5 * level) + 2220);
